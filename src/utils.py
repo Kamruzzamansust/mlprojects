1f670fd90906a0ecc8182652686ed38a91d03480
Kamruzzamansust <kamruzzzaman.sust15@gmail.com>
2023-12-10 09:46:55 -0800

    Hyper parameter train

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,7 +21,7 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(x_train, y_train,x_test,y_test,models):
+def evaluate_models(x_train, y_train,x_test,y_test,models,param):
     try:
         # x_train ,x_test  ,y_train, y_test = train_test_split(
 
@@ -32,8 +32,12 @@ def evaluate_models(x_train, y_train,x_test,y_test,models):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+            #model.fit(x_train,y_train)
 
             y_train_pred = model.predict(x_train)
 
